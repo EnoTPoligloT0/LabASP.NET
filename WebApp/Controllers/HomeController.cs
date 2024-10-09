95645be8748b10e53df9c4306333f1ddcfe1f1fa
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-09 12:13:06 +0200

    Calculator logic

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 using System.Diagnostics;
+using System.Net.Sockets;
 using Microsoft.AspNetCore.Mvc;
 using LabProject.Models;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace LabProject.Controllers;
 
 public class HomeController : Controller
 {
+    // napisz metode age, ktura przyjmuje paramtre z data urodzin i wyswietla wiek w latatch, mesiacach i dniach
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -23,8 +26,45 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Calculator()
+    public IActionResult Calculator(Operators? op, double? x, double? y)
     {
+        // var op = Request.Query["op"];
+        // var x = double.Parse(Request.Query["x"]);
+        // var y = double.Parse(Request.Query["y"]);
+        if (x is null || y is null)
+        {
+            ViewData["Error"] = "Please fill all values";
+            return View("CalculatorError");
+        }
+        
+        
+        double? result = 0.0d;
+        
+        switch (op)
+        {
+            case Operators.Add:
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case Operators.Sub:
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case Operators.Div:
+                result = x / y;
+                ViewBag.Operator = "/";
+                break;
+            case Operators.Mul:
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            default:
+                ViewBag.Error = "Invalid operation";
+                return View("CalculatorError");
+        }
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.y = y;
         return View();
     }
 
@@ -38,4 +78,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
+}
+public enum Operators
+{
+    Add,Sub,Mul,Div
 }
