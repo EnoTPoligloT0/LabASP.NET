0c78c05b9683a8165b4c9d29a72626c2fd401b50
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-12-24 23:56:45 +0100

    Models

---
+++ WebApp/Models/GravityModels/AuthorModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LabProject.Models.GravityEntities;
+
+namespace LabProject.Models.GravityModels;
+
+public class AuthorModel
+{
+    [Required]
+    public string AuthorName { get; set; }
+
+    public List<BookModel> Books { get; set; }
+
+    public ICollection<BookAuthorEntity> BookAuthors { get; set; }
+}
---
+++ WebApp/Models/GravityModels/BookModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabProject.Models.GravityModels;
+
+public class BookModel
+{
+    [Required]
+    public string Title { get; set; }
+
+    [MaxLength(13)]
+    public string Isbn13 { get; set; }
+
+    public int? NumPages { get; set; }
+
+    public DateTime? PublicationDate { get; set; }
+
+    public int AuthorCount { get; set; }
+
+    public int SoldCount { get; set; }
+}
