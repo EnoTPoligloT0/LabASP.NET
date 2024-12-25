5996eee70c0e528c65fbc2e808d00bf76ed0fdeb
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-12-25 07:36:27 +0100

    Book service interface

---
+++ WebApp/Models/Services/IBookService.cs
@@ -0,0 +1,11 @@
+﻿using LabProject.Models.GravityModels;
+
+namespace LabProject.Models.Services;
+
+public interface IBookService
+{
+    (List<BookModel>, int) GetBooks(int pageNumber, int pageSize);
+    BookModel GetBookById(int id);
+    void AddBook(BookModel model);
+    void UpdateBook(int id, BookModel model);
+}
