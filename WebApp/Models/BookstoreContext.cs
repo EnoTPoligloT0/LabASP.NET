d1f86987fe6db3153c669f7b5086b64101bd115b
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-12-25 07:37:08 +0100

    Bookstore context with 4 tables

---
+++ WebApp/Models/BookstoreContext.cs
@@ -0,0 +1,15 @@
+﻿using LabProject.Models.Gravity;
+using LabProject.Models.GravityEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabProject.Models;
+
+public class BookstoreContext : DbContext
+{
+    public DbSet<BookEntity> Books { get; set; }
+    public DbSet<AuthorEntity> Authors { get; set; }
+    public DbSet<BookAuthorEntity> BookAuthors { get; set; }
+    public DbSet<OrderLineEntity> OrderLines { get; set; }
+
+    public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options) { }
+}
