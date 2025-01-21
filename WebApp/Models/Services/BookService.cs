f030a6c1762ba43fc7f36d3c2188b97485af2885
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-01-21 05:15:42 +0100

    GetAuthorsByBookId method

--- WebApp/Models/Services/BookService.cs
+++ WebApp/Models/Services/BookService.cs
@@ -2,6 +2,7 @@
 using LabProject.Models.GravityModels;
 using LabProject.Models.Mappers;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace LabProject.Models.Services
 {
@@ -18,12 +19,21 @@ namespace LabProject.Models.Services
         {
             var totalBooks = _context.Books.Count();
             var books = _context.Books
+                .Select(b => new BookModel
+                {
+                    BookId = b.BookId,
+                    Title = b.Title,
+                    Isbn13 = b.Isbn13,
+                    NumPages = b.NumPages,
+                    PublicationDate = b.PublicationDate,
+                    AuthorCount = _context.BookAuthors.Count(ba => ba.BookId == b.BookId),
+                    SoldCount = _context.OrderLines.Count(ol => ol.BookId == b.BookId)
+                })
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
 
-            var bookModels = books.Select(BookMapper.ToModel).ToList();
-            return (bookModels, totalBooks);
+            return (books, totalBooks);
         }
         public BookModel GetBookById(int id)
         {
@@ -35,6 +45,27 @@ namespace LabProject.Models.Services
 
             return BookMapper.ToModel(bookEntity);
         }
+        public List<AuthorModel> GetAuthorsByBookId(int bookId)
+        {
+            var book = _context.Books
+                .Where(b => b.BookId == bookId)
+                .Include(b => b.BookAuthors)
+                .ThenInclude(ba => ba.Author)
+                .FirstOrDefault();
+
+            if (book == null)
+            {
+                return new List<AuthorModel>();
+            }
+
+
+            var authorModels = book.BookAuthors
+                .Select(ba => AuthorMapper.ToModel(ba.Author))
+                .ToList();
+
+            return authorModels;
+        }
+
 
         public void AddBook(BookModel model)
         {
--- WebApp/Models/Services/IBookService.cs
+++ WebApp/Models/Services/IBookService.cs
@@ -6,6 +6,8 @@ public interface IBookService
 {
     (List<BookModel>, int) GetBooks(int pageNumber, int pageSize);
     BookModel GetBookById(int id);
+    List<AuthorModel> GetAuthorsByBookId(int bookId);
     void AddBook(BookModel model);
     void UpdateBook(int id, BookModel model);
+
 }
